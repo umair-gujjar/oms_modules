45285a36fc9d0f55f62dd43546d9d87f0680928b
umair-gujjar <engrumair_sabir@yahoo.com>
2016-02-03 18:47:04 +0500

    main form fields

--- hr_custom_contract/models.py
+++ hr_custom_contract/models.py
@@ -11,6 +11,9 @@ class hr_custom_contract(models.Model):
 	sr_other = fields.Float('SR/other')
 
 class hr_custom_employee(models.Model):
-	_name = 'hr.custom.employee'
+	#_name = 'hr.custom.employee'
 	_inherit = 'hr.employee'
+	show_engineer = fields.Boolean('Is a Engineer ?')
+	engineer = fields.Char('PEC #', size=64)
+	blood_group = fields.Char('Blood Group', size=10)
 	
